7e38164c7ad45a33823a9a7944278b1c0025d88c
MADARA UCHIHA <madaraotutsuki369@gmail.com>
2026-01-28 18:33:52 +0000

    Paste the following link tags and paste them into the head of your HTML:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,15 @@ export const metadata: Metadata = {
     google: "AHk0YKwgah_gHUL3WxFSIsZvvylxrNrr6MV2d-v9Va8",
   },
 
+  icons: {
+    apple: { url: "/apple-touch-icon.png", sizes: "180x180" },
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+  },
+  manifest: "/site.webmanifest",
+
   openGraph: {
     title: "LuxWalls – Premium Luxury Wallpapers",
     description:
