ef22e1cfbf385cc41513e0b989a8e82102b870ea
MADARA UCHIHA <madaraotutsuki369@gmail.com>
2026-03-04 11:30:53 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,18 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     qualities: [75, 80],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "/**",
+      },
+    ],
   }
 };
 
