58e1e4cba3e81aee422c2e88b1a0259a8d7cb1aa
MADARA UCHIHA <madaraotutsuki369@gmail.com>
2026-03-07 10:50:54 +0000

    Make when user hover or clicks on the walpaper the wallpaper becomes col

--- src/components/sections/WallpaperShowcase.tsx
+++ src/components/sections/WallpaperShowcase.tsx
@@ -31,7 +31,7 @@ export default function WallpaperShowcase() {
                 src={wallpaper.src}
                 alt={`${wallpaper.title} - LuxWalls Wallpaper Sample`}
                 fill
-                className="object-cover transition-transform duration-[1.5s] ease-in-out group-hover:scale-110"
+                className="object-cover transition-all duration-[1.5s] ease-in-out group-hover:scale-110 group-hover:grayscale group-active:grayscale"
                 sizes="(max-width: 640px) 50vw, (max-w: 1024px) 33vw, 16vw"
               />
               
