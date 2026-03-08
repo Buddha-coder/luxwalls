f2e6456a29a1e1fd2ecb95efa46eb313ac5d7ce1
MADARA UCHIHA <madaraotutsuki369@gmail.com>
2026-03-08 06:20:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-563094161-6c93f",
+  "appId": "1:1036938703442:web:3d6134d9aba3f0dafc658b",
+  "apiKey": "AIzaSyDOFF6BRhai5ABw0rGuFLnQa7SVXGYueak",
+  "authDomain": "studio-563094161-6c93f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1036938703442"
+};
