95926511c920855949c87546fe55baffccf2234b
MADARA UCHIHA <madaraotutsuki369@gmail.com>
2026-03-10 14:35:54 +0000

    adeed adsense code un head tag

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-563094161-6c93f",
-  "appId": "1:1036938703442:web:3d6134d9aba3f0dafc658b",
-  "apiKey": "AIzaSyDOFF6BRhai5ABw0rGuFLnQa7SVXGYueak",
-  "authDomain": "studio-563094161-6c93f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1036938703442"
+  apiKey: "AIzaSyDOFF6BRhai5ABw0rGuFLnQa7SVXGYueak",
+  authDomain: "studio-563094161-6c93f.firebaseapp.com",
+  databaseURL: "https://studio-563094161-6c93f-default-rtdb.firebaseio.com",
+  projectId: "studio-563094161-6c93f",
+  storageBucket: "studio-563094161-6c93f.firebasestorage.app",
+  messagingSenderId: "1036938703442",
+  appId: "1:1036938703442:web:cf514b722f194e55fc658b"
 };
